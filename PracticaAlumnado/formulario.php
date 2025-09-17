ae05c62d54bd7c5a559fb86728acd6dee4a07969
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-09-17 12:39:08 +0200

    Añadir atributos class para estilos

--- PracticaAlumnado/formulario.php
+++ PracticaAlumnado/formulario.php
@@ -63,24 +63,24 @@
     <?php
         $conexion = mysqli_connect("localhost","alumno1","alumno1","formulario_practica")
             or die("Problemas al establecer conexion");
-        $registros = mysqli_query($conexion, "Select * from alumnos");
-        if ($registros != null){
-            echo "<div class='tablaAlumnos'>";
-            echo "<table>";
+        $registros = mysqli_query($conexion, "Select * from alumnos order by curso, nombre");
+        if (mysqli_num_rows($registros) > 0){
+            echo "<div>";
+            echo "<table class='tablaAlumnos'>";
             echo"<tr>";
-            echo "<th>Nombre</th>";
-            echo "<th>Apellidos</th>";
-            echo "<th>Fecha de Nacimiento</th>";
-            echo "<th>Curso</th>";
-            echo "<th>Email</th>";
+            echo "<th class='thAlumnos'>Nombre</th>";
+            echo "<th class='thAlumnos'>Apellidos</th>";
+            echo "<th class='thAlumnos'>Fecha de Nacimiento</th>";
+            echo "<th class='thAlumnos'>Curso</th>";
+            echo "<th class='thAlumnos'>Email</th>";
             echo "</tr>";
             foreach ($registros as $item){
                 echo "<tr>";
-                echo "<td>{$item['nombre']}</td>";
-                echo "<td>{$item['apellidos']}</td>";
-                echo "<td>{$item['fecha_nacimiento']}</td>";
-                echo "<td>{$item['curso']}</td>";
-                echo "<td>{$item['email']}</td>";
+                echo "<td class='tdAlumnos'>{$item['nombre']}</td>";
+                echo "<td class='tdAlumnos'>{$item['apellidos']}</td>";
+                echo "<td class='tdAlumnos'>{$item['fecha_nacimiento']}</td>";
+                echo "<td class='tdAlumnos'>{$item['curso']}</td>";
+                echo "<td class='tdAlumnos'>{$item['email']}</td>";
                 echo "</tr>";
             }
             echo "</table>";
