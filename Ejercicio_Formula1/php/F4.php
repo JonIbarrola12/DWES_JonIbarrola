e3096cdf9eabbbe871a17ff4abadc3e5ea2ddce0
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-10-14 11:02:30 +0200

    Clase F4

---
+++ Ejercicio_Formula1/php/F4.php
@@ -0,0 +1,67 @@
+<?php
+    class F4 extends Monoplaza{
+        private string $paisCategoria;
+
+        public function __construct(string $pNombrePiloto,string $pNacionalidadPiloto, int $pNumeroMonoplaza, string $pEscuderiaPiloto, int $pPuntosPiloto,string $pPaisCategoria ){
+            parent::__construct($pNombrePiloto,$pNacionalidadPiloto,$pNumeroMonoplaza,$pEscuderiaPiloto,$pPuntosPiloto);
+            $this->paisCategoria = $pPaisCategoria;
+        }
+
+        public function setPaisCategoria(bool $pPaisCategoria){
+            $this->paisCategoria = $pPaisCategoria;
+
+        }
+        public function getPaisCategoria(){
+            return $this->paisCategoria;
+        }
+
+        public function obtenerPuntuacion(int $posicion,bool $vueltaRapida){
+            if (posicionValida($posicion)){
+                switch($posicion){
+                    case 1 : 
+                        $this->puntosPiloto += 25;
+                        break;
+                    case 2:
+                        $this->puntosPiloto += 18;
+                        break;
+                    case 3:
+                        $this->puntosPiloto += 15;
+                        break;
+                    case 4:
+                        $this->puntosPiloto += 12;
+                        break;
+                    case 5:
+                        $this->puntosPiloto += 10;
+                        break;
+                    case 6:
+                        $this->puntosPiloto += 8;
+                        break;
+                    case 7:
+                        $this->puntosPiloto += 6;
+                        break;
+                    case 8:
+                        $this->puntosPiloto += 4;
+                        break;
+                    case 9:
+                        $this->puntosPiloto += 2;
+                        break;
+                    case 10:
+                        $this->puntosPiloto += 1;
+                        break;
+                }
+            }else{
+                echo "Poscion no valida";
+            }
+        }
+        public function posicionValida(int $posicion){
+            if($posicion > 0 && posicion < 31){
+                return true;
+            }
+            return false;
+        }
+        public function subirCategoria(string $pNombreAcademia){
+            $f3 = new F3($pNombrePiloto, $pNacionalidadPiloto, $pNumeroMonoplaza, $pEscuderiaPiloto, 0, $pNombreAcademia);
+            return $f3;
+        }
+    }
+    ?>
